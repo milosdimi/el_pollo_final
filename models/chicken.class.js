f86a6721b412f5ca65cf936cea170db0e5c3f7cf
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-09-18 23:55:11 +0200

    17 - Himmel einfügen

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,7 @@
 class Chicken extends MovableObject {
 
+    y = 295;
+
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
--- models/world.class.js
+++ models/world.class.js
@@ -10,10 +10,10 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0,100),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
+        new BackgroundObject('img/5_background/layers/air.png', 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 0),
     ];
     canvas;
     ctx;
@@ -28,10 +28,10 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // clear canvas
 
+        this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.clouds);
-        this.addObjectsToMap(this.backgroundObjects);
 
 
 
