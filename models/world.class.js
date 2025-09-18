65734a262a78c41e5d0004db2d3aac30a77f6465
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-09-18 22:50:00 +0200

    10 - requestAnimationFrame()

--- models/world.class.js
+++ models/world.class.js
@@ -5,10 +5,25 @@ class World {
         new Chicken(),
         new Chicken()
     ];
+    canvas;
+    ctx;
 
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
 
 
     draw() {
-        console.log('World wird gezeichnet');
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // clear canvas
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        });
+    
+    
+    
+        requestAnimationFrame(() => this.draw());
     }
 }
