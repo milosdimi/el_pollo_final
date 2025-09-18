c84e5aec7a1891d52ccbdedbe670a5393d23c863
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-09-19 00:00:09 +0200

    18 - Objekte skalieren

--- models/character.class.js
+++ models/character.class.js
@@ -1,13 +1,14 @@
 class Character extends MovableObject {
 
-
+    height = 280;
+    y = 155;
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
 
     }
 
     jump() {
-        
+
     }
 
 }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,7 +1,8 @@
 class Chicken extends MovableObject {
 
-    y = 295;
-
+    y = 360;
+    height = 60;
+    width = 80;
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
