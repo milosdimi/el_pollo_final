11ef5b6b3f4783507b6119e88136690c676febcf
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-09-21 22:59:51 +0200

    02 - ImageCache

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -3,11 +3,18 @@ class Cloud extends MovableObject {
     width = 500;
     height = 250;
 
+
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
+
         this.x = Math.random() * 500; // zufällige Position
-        this.speed = 0.03 + Math.random() * 0.1;
+        this.animate();
     }
 
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60);
+    }
 
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -5,12 +5,25 @@ class MovableObject {
     height = 150;
     width = 100;
     speed = 0.15;
+    imageCache = {};
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
+
+    /**
+     * 
+     * @param {Array} arr  - ['path1', 'path2', ...]
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = path;
+        });
+    }
 
 
     moveRight() {
@@ -20,6 +33,6 @@ class MovableObject {
     moveLeft() {
 
     }
-    
+
 
 }
