b37a269375a204b473b258709dc41cb233ab6972
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-09-22 22:32:39 +0200

    14 - Character optimieren

---
+++ models/level.class.js
@@ -0,0 +1,16 @@
+class Level {
+    enemies;
+    clouds;
+    backgroundObjects;
+    level_end_x = 719 * 3; // 4 Bildschirme lang
+
+    constructor(enemies, clouds, backgroundObjects) {
+        this.enemies = enemies;
+        this.clouds = clouds;
+        this.backgroundObjects = backgroundObjects;
+    }
+
+
+
+
+}
--- models/world.class.js
+++ models/world.class.js
@@ -1,42 +1,6 @@
 class World {
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
-    clouds = [
-        new Cloud(),
-        new Cloud()
-    ];
-    backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/air.png', -719),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', -719),
-
-        new BackgroundObject('img/5_background/layers/air.png', 0),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 0),
-        new BackgroundObject('img/5_background/layers/air.png', 719),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719),
-
-        new BackgroundObject('img/5_background/layers/air.png', 719*2),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719*2),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719*2),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719*2),
-        new BackgroundObject('img/5_background/layers/air.png', 719*3),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719*3),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719*3),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719*3),
-
-
-
-
-    ];
+    level = level1;
     canvas;
     ctx;
     keyboard;
@@ -54,10 +18,10 @@ class World {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // clear canvas
 
         this.ctx.translate(this.camera_x, 0); // camera movement
-        this.addObjectsToMap(this.backgroundObjects);
+        this.addObjectsToMap(this.level.backgroundObjects);
         this.addToMap(this.character);
-        this.addObjectsToMap(this.enemies);
-        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.level.enemies);
+        this.addObjectsToMap(this.level.clouds);
         this.ctx.translate(-this.camera_x, 0); // reverse camera movement
 
         requestAnimationFrame(() => this.draw());
