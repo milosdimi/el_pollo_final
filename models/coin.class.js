42a46bd43171f2cb284c8917201a2d180a4e0515
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-10-01 20:14:30 +0200

    coins position edit

--- models/coin.class.js
+++ models/coin.class.js
@@ -2,7 +2,7 @@ class Coin extends MovableObject {
     height = 90;
     width = 90;
 
-    offset = { top: 28, bottom: 28, left: 28, right: 28};
+    offset = { top: 28, bottom: 28, left: 28, right: 28 };
 
     IMAGES_COIN = [
         'img/8_coin/coin_1.png',
@@ -14,10 +14,10 @@ class Coin extends MovableObject {
         this.x = x;
         this.y = y;
         this.loadImages(this.IMAGES_COIN);
+        
+        this.x += (Math.random() * 10) - 1;
+        this.y += (Math.random() * 24) - 100;
 
-
-        setInterval(() => {
-            this.playAnimation(this.IMAGES_COIN);
-        }, 1000 / 3);
+        setInterval(() => this.playAnimation(this.IMAGES_COIN), 1000 / 3);
     }
 }
